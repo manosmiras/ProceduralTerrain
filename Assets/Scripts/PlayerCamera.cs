39ffdc6c4d6a48f4ec462df5313a4be55fccdde5
Manos Miras <manosmiras@outlook.com>
2026-04-12 21:35:45 +0300

    wip paths

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -10,17 +10,11 @@ public class PlayerCamera : MonoBehaviour
     private float3[] _cameraPath;
     private TerrainChunk _chunk;
     private int _pathIndex;
-    private const int PathSimplification = 64;
     private float _progress;
     
     private void OnEnable()
     {
         ProceduralTerrain.Instance.OnTerrainGenerated += GenerateCameraPath;
-    }
-
-    private void OnDisable()
-    {
-        ProceduralTerrain.Instance.OnTerrainGenerated -= GenerateCameraPath;
     }
 
     private void Start()
@@ -32,45 +26,59 @@ public class PlayerCamera : MonoBehaviour
     {
         Debug.Log("Generating camera path");
         // TODO: Take lod into account? 
-        var terrain = ProceduralTerrain.Instance;
-        var chunkCount = terrain.TerrainChunks.Count;
-        var pointsPerChunk = (terrain.ChunkSize + PathSimplification - 1) / PathSimplification;
-        _cameraPath = new float3[pointsPerChunk * chunkCount];
-        for (var chunkIndex = 0; chunkIndex < chunkCount; chunkIndex++)
+        _cameraPath = GetPathForChunk(ProceduralTerrain.Instance.TerrainChunks[0], 4);
+        foreach (var p in _cameraPath)
         {
-            var terrainChunk = terrain.TerrainChunks[chunkIndex];
-            var middleX = terrain.ChunkSize / 2;
-            
-            var pathIndex = 0;
-            var baseIndex = chunkIndex * pointsPerChunk;
-            for (var z = terrain.ChunkSize - 1; z >= 0; z -= PathSimplification)
-            {
-                var index = middleX + z * terrain.ChunkSize;
-                _cameraPath[baseIndex + pathIndex] =  terrainChunk.transform.position + terrainChunk.MeshData.Vertices[index];
-                pathIndex++;
-            }
+            new GameObject("Camera Path Point").transform.position = p;
         }
 
         _splinePath = new Spline();
         _splinePath.AddRange(_cameraPath);
         Debug.Log($"Camera path has {_cameraPath.Length} points");
     }
+
+    private float3[] GetPathForChunk(TerrainChunk chunk, int samples)
+    {
+        var path = new float3[samples + 1];
+        var position = chunk.transform.position;
+        var step = ProceduralTerrain.Instance.ChunkSize / samples;
+        var start = -samples / 2;
+        var end = samples / 2;
+        var index = 0;
+        for (var offset = start; offset <= end; offset++)
+        {
+            var sample = position + new Vector3(0, 1000f, step * offset);
+            // Shove in / out of chunk for successful raycast
+            if (offset == start)
+            {
+                sample.z += 0.5f;
+            } else if (offset == end)
+            {
+                sample.z -= 0.5f;
+            }
+            var isHit = Physics.Raycast(sample, Vector3.down, out var hit, 10000f);
+            Debug.Log(isHit);
+            path[index] = hit.point;
+            index++;
+        }
+        return path;
+    }
     
-    private void Update()
+    /*private void Update()
     {
         for (var i = 0; i < _cameraPath.Length - 1; i++)
         {
             Debug.DrawLine(_cameraPath[i], _cameraPath[i + 1], Color.red);
         }
         var dist = Vector3.Distance(ProceduralTerrain.Instance.LastChunk.transform.position, transform.position);
-        if (dist <= ProceduralTerrain.Instance.ChunkSize / 2)
+        if (dist <= ProceduralTerrain.Instance.ChunkSize / 2f)
         {
             ProceduralTerrain.Instance.AddTerrainChunk();
         }
-        Debug.Log($"Distance to last chunk: {dist}");
-    }
+        //Debug.Log($"Distance to last chunk: {dist}");
+    }*/
     
-    private void LateUpdate()
+    /*private void LateUpdate()
     {
         if (_splinePath == null || _splinePath.Count < 2)
             return;
@@ -82,7 +90,7 @@ public class PlayerCamera : MonoBehaviour
 
         transform.position = position;
         transform.rotation = Quaternion.LookRotation(tangent, up);
-    }
+    }*/
     
     private float GetApproxSplineLength()
     {
